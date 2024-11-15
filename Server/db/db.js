5be2ce08dd2c48d30bc2e0ce9d04f51a60ab1bf2
Adarshk6600 <adarshk6600@outlook.com>
2024-11-16 02:52:58 +0530

    push

--- Server/db/db.js
+++ Server/db/db.js
@@ -1,11 +1,12 @@
 env.config()
 import mongoose from "mongoose";
 import env from 'dotenv'
+import { DB_NAME } from '../constants.js'; 
 
 const dbConnect = async () => {
   try {
-    console.log(`${process.env.MONGO_DB_URI} is the url`);
-    await mongoose.connect(`${process.env.MONGO_DB_URI}`)
+    console.log(`${process.env.MONGO_DB_URI}/${DB_NAME}`);
+    await mongoose.connect(`${process.env.MONGO_DB_URI}/${DB_NAME}`)
     console.log('database connection successful');
     
   } catch (error) {
